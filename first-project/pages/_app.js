7b4ffa0f8b110bea9db40c0e681f10ca212af1d0
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-09-30 23:26:48 +0330

    usage add global things in _app.js

--- first-project/pages/_app.js
+++ first-project/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <p>Header</p>
+  <Component {...pageProps} />
+  <p>Footer</p>
+  </>
+)
 }
 
 export default MyApp
