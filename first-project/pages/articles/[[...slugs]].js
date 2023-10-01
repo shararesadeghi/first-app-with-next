ec8837d6e2eb726b1e8aa813663afd9f6c9cb2d9
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-01 14:59:49 +0330

    use catch all routes

---
+++ first-project/pages/articles/[[...slugs]].js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Article = () => {
+
+    const router = useRouter();
+    const slugs =router.query || [];
+
+    if(slugs.length > 1){
+        return (<h3>This article is written by {slugs[0]} at {slugs[1]}/{slugs[2]}/{slugs[3]} . and the name of article is {slugs[4]}</h3>);
+    }
+    return (
+        <h1>
+           Article 
+        </h1>
+    );
+};
+
+export default Article;
