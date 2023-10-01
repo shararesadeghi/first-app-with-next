bc55c0fba654944338a6517afa6a6a47b77153f4
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-01 16:06:09 +0330

    use styled jsx in next

--- first-project/pages/courses/react.js
+++ first-project/pages/courses/react.js
@@ -1,6 +1,21 @@
 
 function React(){
-    return <h1>React</h1>
+    return (
+        <div>
+            <h1>React Courses</h1>
+            <p>You should learn react before Next.js</p>
+            <h4>Instractor: Sharareh Sadeghi</h4>
+            <style jsx>{`
+                h1{
+                color: red;
+                background-color: silver;
+                }
+                p{
+                    width:500px;
+                }
+            `}</style>
+        </div>
+    )
 }
 
 export default React;
